e27ea33f8cfc48758034a0b99a5be2e1204dcd9d
Yash Katyan <yashkatyan@Yashs-MacBook-Air-2.local>
2024-10-04 17:47:45 +0530

    day 2 more

--- mygolang-lco/02variables/main.go
+++ mygolang-lco/02variables/main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Variables")
+	var username string = "Yash"
+	fmt.Println(username)
+	fmt.Printf("The Variable is of Type : %T \n", username)
+
+	var isLoggedin bool = true
+	fmt.Println(isLoggedin)
+	fmt.Printf("The Variable is of Type : %T \n", isLoggedin)
+
+	var smallVal uint = 255
+	fmt.Println(smallVal)
+	fmt.Printf("The Variable is of Type : %T \n", smallVal)
+
+	var smallFloat float64 = 23.877878990099
+	fmt.Println(smallFloat)
+	fmt.Printf("The Variable is of Type : %T \n", smallFloat)
+
+	//default values
+	var anotherVar string
+	fmt.Println(anotherVar)
+	fmt.Printf("The Variable is of Type : %T \n", anotherVar)
+
+	// implicit type declaration
+	var website = "upsc.gov.in"
+	fmt.Println(website)
+
+	// no var style
+
+	numberOfUser := 300000
+	fmt.Println(numberOfUser)
+
 }
